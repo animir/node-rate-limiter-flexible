c8cf8e88609181818b95ad8527f6567d0bcc7e18
Roman Voloboev <animir@ya.ru>
2018-05-23 19:08:35 +1000

    Added: rate limiter cluster

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const RateLimiterRedis = require('./lib/RateLimiterRedis');
+const RateLimiterCluster = require('./lib/RateLimiterCluster');
 const RateLimiterMemory = require('./lib/RateLimiterMemory');
 
 module.exports = {
   RateLimiterRedis,
-  RateLimiterMemory
+  RateLimiterMemory,
+  RateLimiterCluster
 };
