ad4c058e753787799af8759608fa4c7899973602
Roman Voloboev <animir@ya.ru>
2018-06-21 16:51:01 +1000

    Fixed: mysql expiration

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const RateLimiterRedis = require('./lib/RateLimiterRedis');
 const RateLimiterMongo = require('./lib/RateLimiterMongo');
+const RateLimiterMySQL = require('./lib/RateLimiterMySQL');
 const { RateLimiterClusterMaster, RateLimiterCluster } = require('./lib/RateLimiterCluster');
 const RateLimiterMemory = require('./lib/RateLimiterMemory');
 
 module.exports = {
   RateLimiterRedis,
   RateLimiterMongo,
+  RateLimiterMySQL,
   RateLimiterMemory,
   RateLimiterClusterMaster,
   RateLimiterCluster,
