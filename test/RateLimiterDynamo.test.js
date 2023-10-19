a4f65fec636c717376ed408d442d66375584536b
daniele <zeolla.d@gmail.com>
2023-10-19 16:41:51 +0200

    Continue test dynamo

--- test/RateLimiterDynamo.test.js
+++ test/RateLimiterDynamo.test.js
@@ -1,9 +1,27 @@
 const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { expect } = require('chai');
 const { describe, it, beforeEach } = require('mocha');
+const RateLimiterDynamo = require('../lib/RateLimiterDynamo');
 
 describe('RateLimiterDynamo with fixed window', function RateLimiterDynamoTest() {
     this.timeout(2000);
 
-    let dynamoClient = new DynamoDBClient({region: 'eu-central-1'});
+    const dynamoClient = new DynamoDBClient({region: 'eu-central-1'});
+    
+    it('instantiate DynamoDb client', (done) => {
+        expect(dynamoClient).to.not.equal(null);
+        done();
+    });
+
+    it('rate limiter dynamo init', (done) => {
+        const rateLimiter = new RateLimiterDynamo({
+            storeClient: dynamoClient
+        },
+        () => {
+            done();
+        }
+        );
+        //console.log(rateLimiter);
+    });
 
 })
