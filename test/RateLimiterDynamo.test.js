79f37ec9c1ce782da2f3eece7c51f405562aef8d
daniele <zeolla.d@gmail.com>
2023-10-20 12:06:40 +0200

    Fixed create table

--- test/RateLimiterDynamo.test.js
+++ test/RateLimiterDynamo.test.js
@@ -1,12 +1,20 @@
-const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const AWS = require('@aws-sdk/client-dynamodb');
+const {DynamoDBClient} = require('@aws-sdk/client-dynamodb');
 const { expect } = require('chai');
 const { describe, it, beforeEach } = require('mocha');
 const RateLimiterDynamo = require('../lib/RateLimiterDynamo');
 
 describe('RateLimiterDynamo with fixed window', function RateLimiterDynamoTest() {
-    this.timeout(2000);
+    this.timeout(5000);
 
-    const dynamoClient = new DynamoDBClient({region: 'eu-central-1'});
+    const dynamoClient = new AWS.DynamoDB({region: 'eu-central-1'});
+    /*
+    const client2 = new DynamoDBClient({region: 'eu-central-1'});
+    console.log(dynamoClient)
+    console.log("V3")
+    console.log(client2)
+    */
+    
     
     it('instantiate DynamoDb client', (done) => {
         expect(dynamoClient).to.not.equal(null);
@@ -17,11 +25,12 @@ describe('RateLimiterDynamo with fixed window', function RateLimiterDynamoTest()
         const rateLimiter = new RateLimiterDynamo({
             storeClient: dynamoClient
         },
-        () => {
+        (data) => {
+            console.log(data);
             done();
         }
         );
-        //console.log(rateLimiter);
+        
     });
-
+    
 })
