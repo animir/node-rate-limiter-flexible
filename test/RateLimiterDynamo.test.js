615ac3e8dbb5ea505a965ba5c597d8865dc2a015
daniele <zeolla.d@gmail.com>
2023-10-24 11:56:52 +0200

    Fix: error in _upsert and new test

--- test/RateLimiterDynamo.test.js
+++ test/RateLimiterDynamo.test.js
@@ -13,30 +13,72 @@ describe('RateLimiterDynamo with fixed window', function RateLimiterDynamoTest()
         done();
     });
 
-    it('rate limiter dynamo init', (done) => {
-        const rateLimiter = new RateLimiterDynamo({
-            storeClient: dynamoClient
-        },
-        (data) => {
-            done();
-        }
-        );
-        
-    });
-
     it('get item from DynamoDB', (done) => {
         
+        const testKey = 'test';
         const rateLimiter = new RateLimiterDynamo({
             storeClient: dynamoClient
         },
         () => {
-            rateLimiter.get('test')
+            rateLimiter.set(testKey, 999, 10000)
+            .then((data) => {
+                rateLimiter.get(testKey)
+                .then((response) => {
+                    expect(response).to.not.equal(null);
+                    done();
+                })
+                .catch((err) => {
+                    done(err);
+                });
+            })
+            .catch((err) => {
+                done(err);
+            })
+        }
+        );
+    });
+
+    it('get NOT existing item from DynamoDB', (done) => {
+        
+        const testKey = 'not_existing';
+        const rateLimiter = new RateLimiterDynamo({
+            storeClient: dynamoClient
+        },
+        () => {
+            rateLimiter.get(testKey)
             .then((response) => {
+                expect(response).to.equal(null);
                 done();
             })
             .catch((err) => {
                 done(err);
             });
+        }
+        );
+    });
+
+    it('delete item from DynamoDB', (done) => {
+        
+        const testKey = 'test';
+        const rateLimiter = new RateLimiterDynamo({
+            storeClient: dynamoClient
+        },
+        () => {
+            rateLimiter.set(testKey, 999, 10000)
+            .then((data) => {
+                rateLimiter.delete(testKey)
+                .then((response) => {
+                    console.log(response)
+                    expect(response).to.not.equal(null);
+                    done();
+                })
+                .catch((err) => {
+                    done(err);
+                });
+            })
+            .catch((err) => {
+                done(err);
+            })
         }
         );
     });
