0f41ae659f1bbf5a625b2f2706a6b7f21976eedd
daniele <zeolla.d@gmail.com>
2023-10-30 13:17:20 +0100

    Added tests

--- test/RateLimiterDynamo.test.js
+++ test/RateLimiterDynamo.test.js
@@ -4,10 +4,10 @@ const { describe, it, beforeEach } = require('mocha');
 const RateLimiterDynamo = require('../lib/RateLimiterDynamo');
 
 describe('RateLimiterDynamo with fixed window', function RateLimiterDynamoTest() {
-    this.timeout(5000);
+    this.timeout(10000);
 
     const dynamoClient = new DynamoDB({region: 'eu-central-1'});
-    /*
+    
     it('instantiate DynamoDb client', (done) => {
         expect(dynamoClient).to.not.equal(null);
         done();
@@ -106,7 +106,7 @@ describe('RateLimiterDynamo with fixed window', function RateLimiterDynamoTest()
         }
         );
     });
-    */
+    
     it('consume 1 point', (done) => {
         const testKey = 'consume1';
 
@@ -119,7 +119,7 @@ describe('RateLimiterDynamo with fixed window', function RateLimiterDynamoTest()
             rateLimiter.consume(testKey)
                 .then((result) => {
                     expect(result.consumedPoints).to.equal(1);
-                    //rateLimiter.delete(testKey);
+                    rateLimiter.delete(testKey);
                     done();
                 })
                 .catch((err) => {
@@ -129,5 +129,146 @@ describe('RateLimiterDynamo with fixed window', function RateLimiterDynamoTest()
         });
         
     });
+
+    it('rejected when consume more than maximum points', (done) => {
+        const testKey = 'consumerej';
     
-})
+        const rateLimiter = new RateLimiterDynamo({
+            storeClient: dynamoClient,
+            points: 1,
+            duration: 5
+        },
+        () => {
+            rateLimiter.consume(testKey, 2)
+                .then((result) => {
+                    expect(result.consumedPoints).to.equal(2);
+                    done(Error('must not resolve'));
+                })
+                .catch((err) => {
+                    expect(err.consumedPoints).to.equal(2);
+                    done();
+                });
+
+        });
+    });
+
+    it('blocks key for block duration when consumed more than points', (done) => {
+        const testKey = 'block';
+        
+        const rateLimiter = new RateLimiterDynamo({
+            storeClient: dynamoClient,
+            points: 1,
+            duration: 1,
+            blockDuration: 2
+        },
+        () => {
+            rateLimiter.consume(testKey, 2)
+                .then((result) => {
+                    expect(result.consumedPoints).to.equal(2);
+                    done(Error('must not resolve'));
+                })
+                .catch((err) => {
+                    expect(err.msBeforeNext > 1000).to.equal(true);
+                    done();
+                });
+
+        });
+        
+      });
+
+    it('return correct data with _getRateLimiterRes', () => {
+        const testKey = 'test';
+        
+        const rateLimiter = new RateLimiterDynamo({
+            storeClient: dynamoClient,
+            points: 5,
+        },
+        () => {
+            
+            const res = rateLimiter._getRateLimiterRes(
+                'test',
+                1,
+                { key: 'test', points: 3, expire: Date.now() + 1000}
+                );
+
+            expect(res.msBeforeNext <= 1000 && 
+                        res.consumedPoints === 3 && 
+                        res.isFirstInDuration === false && 
+                        res.remainingPoints === 2
+                    ).to.equal(true);
+
+        });
+    });
+
+    it('get points', (done) => {
+        const testKey = 'get';
+    
+        const rateLimiter = new RateLimiterDynamo({
+            storeClient: dynamoClient,
+            points: 5,
+        },
+        () => {
+            
+            rateLimiter.set(testKey, 999, 10000)
+                .then((data) => {
+                    rateLimiter.get(testKey)
+                        .then((response) => {
+                            expect(response.consumedPoints).to.equal(999);
+                            rateLimiter.delete(testKey);
+                            done();
+                        })
+                        .catch((err) => {
+                            done(err);
+                        });
+                })
+                .catch((err) => {
+                    done(err);
+                });
+
+        });
+
+    });
+
+    it('get points return NULL if key is not set', (done) => {
+        const testKey = 'getnull';
+        
+        const rateLimiter = new RateLimiterDynamo({
+            storeClient: dynamoClient,
+            points: 5,
+        },
+        () => {
+            
+            rateLimiter.get(testKey)
+                .then((response) => {
+                    expect(response).to.equal(null);
+                    done();
+                })
+                .catch((err) => {
+                    done(err);
+                });
+        });
+        
+    });
+
+    it('delete returns false, if there is no key', (done) => {
+        const testKey = 'getnull3';
+        
+        const rateLimiter = new RateLimiterDynamo({
+            storeClient: dynamoClient,
+            points: 5,
+        },
+        () => {
+            
+            rateLimiter.delete(testKey)
+                .then((response) => {
+                    expect(response).to.equal(false);
+                    done();
+                })
+                .catch((err) => {
+                    done(err);
+                });
+        });
+        
+    });
+    
+});
