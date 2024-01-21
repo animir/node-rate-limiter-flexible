e8a313b73d2796f28ff7de20156b4a2c034d20c7
Roman <animir@ya.ru>
2024-01-21 15:38:15 +0100

    fix eslint errors

--- test/component/RateLimiterQueueError.test.js
+++ test/component/RateLimiterQueueError.test.js
@@ -1,13 +1,13 @@
-import mocha from "mocha";
-import { expect } from "chai";
+import mocha from 'mocha';
+import { expect } from 'chai';
 import RateLimiterQueueError from '../../lib/component/RateLimiterQueueError';
 
 const { describe, it } = mocha;
 
 describe('RateLimiterQueueError', () => {
   it('supports extra argument in constructor', (done) => {
-  const err = new RateLimiterQueueError('test', 'extra');
-  expect(err.extra).to.equal('extra');
-  done();
+    const err = new RateLimiterQueueError('test', 'extra');
+    expect(err.extra).to.equal('extra');
+    done();
   });
 });
