ac8c5c565a6640c3211f61ab1fac7c89df8ab918
Roman <animir@ya.ru>
2026-04-03 17:22:24 +0200

    #356 fix tests

--- test/RateLimiterAbstract.test.js
+++ test/RateLimiterAbstract.test.js
@@ -29,31 +29,31 @@ describe('RateLimiterAbstract', function () {
   it('throws error if points is not set', () => {
     expect(() => {
       new RateLimiterAbstract({ duration: 1 });
-    }).to.throw('points must be set and must be a number');
+    }).to.throw('points must be set and must be a finite number');
   });
 
   it('throws error if points is null', () => {
     expect(() => {
       new RateLimiterAbstract({ points: null, duration: 1 });
-    }).to.throw('points must be set and must be a number');
+    }).to.throw('points must be set and must be a finite number');
   });
 
   it('throws error if points is a string', () => {
     expect(() => {
       new RateLimiterAbstract({ points: '10', duration: 1 });
-    }).to.throw('points must be set and must be a number');
+    }).to.throw('points must be set and must be a finite number');
   });
 
   it('throws error if points is an object', () => {
     expect(() => {
       new RateLimiterAbstract({ points: {}, duration: 1 });
-    }).to.throw('points must be set and must be a number');
+    }).to.throw('points must be set and must be a finite number');
   });
 
   it('throws error if points is a boolean', () => {
     expect(() => {
       new RateLimiterAbstract({ points: true, duration: 1 });
-    }).to.throw('points must be set and must be a number');
+    }).to.throw('points must be set and must be a finite number');
   });
 
   it('accepts positive number as duration value', () => {
@@ -69,31 +69,31 @@ describe('RateLimiterAbstract', function () {
   it('throws error if duration is not set', () => {
     expect(() => {
       new RateLimiterAbstract({ points: 4 });
-    }).to.throw('duration must be set and must be a non-negative number');
+    }).to.throw('duration must be set and must be a finite, non-negative number');
   });
 
   it('throws error if duration is negative', () => {
     expect(() => {
       new RateLimiterAbstract({ points: 4, duration: -1 });
-    }).to.throw('duration must be set and must be a non-negative number');
+    }).to.throw('duration must be set and must be a finite, non-negative number');
   });
 
   it('throws error if duration is null', () => {
     expect(() => {
       new RateLimiterAbstract({ points: 4, duration: null });
-    }).to.throw('duration must be set and must be a non-negative number');
+    }).to.throw('duration must be set and must be a finite, non-negative number');
   });
 
   it('throws error if duration is a string', () => {
     expect(() => {
       new RateLimiterAbstract({ points: 4, duration: '5' });
-    }).to.throw('duration must be set and must be a non-negative number');
+    }).to.throw('duration must be set and must be a finite, non-negative number');
   });
 
   it('throws error if duration is an object', () => {
     expect(() => {
       new RateLimiterAbstract({ points: 4, duration: {} });
-    }).to.throw('duration must be set and must be a non-negative number');
+    }).to.throw('duration must be set and must be a finite, non-negative number');
   });
 
   describe('parseKey', () => {
